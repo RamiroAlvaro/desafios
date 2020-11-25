c0f96c1ba089740d34bb80cfed35503e2ace7a8f
Ramiro Alvaro <ramiroalvaro@hotmail.com>
2020-11-25 06:12:42 -0300

    refactoring and test

---
+++ backend/common_level.py
@@ -0,0 +1,49 @@
+import json
+import sys
+
+
+def read(json_file):
+    with open(json_file, 'r') as f:
+        data = json.load(f)
+        return data
+
+
+def write(json_name, result):
+    with open(json_name, 'w') as f:
+        json.dump({'carts': result}, f)
+
+
+def articles_id_price(articles):
+    return {item['id']: item['price'] for item in articles}
+
+
+def discounted_items(discounts, dict_articles_price):
+    for discount in discounts:
+        id_ = discount['article_id']
+        type_ = discount['type']
+        value = discount['value']
+        if type_ == 'amount':
+            dict_articles_price[id_] -= value
+        elif type_ == 'percentage':
+            dict_articles_price[id_] -= (dict_articles_price[id_] * value / 100)
+
+
+def delivery_fees_range_prices(delivery_fees):
+    dict_delivery_fees_range_prices = {}
+
+    for element_dict in delivery_fees:
+        max_price = element_dict['eligible_transaction_volume']['max_price']
+        min_price = element_dict['eligible_transaction_volume']['min_price']
+        if not max_price:
+            rank = range(min_price, sys.maxsize ** 10)
+        else:
+            rank = range(min_price, max_price)
+        dict_delivery_fees_range_prices[element_dict['price']] = rank
+    return dict_delivery_fees_range_prices
+
+
+def delivery_price(total_price, dict_delivery_fees_range_prices):
+    for key, value in dict_delivery_fees_range_prices.items():
+        if total_price in value:
+            return key
+    return 0
