99745c4efb8e727e6e0ed8c115b472bc675a3785
Ramiro Alvaro <ramiroalvaro@hotmail.com>
2020-12-12 12:41:11 -0300

    Desafio proximo cafe com python

--- maximum_subarray.py
+++ maximum_subarray.py
@@ -7,12 +7,14 @@ For example, given the array [−2,1,−3,4,−1,2,1,−5,4], the contiguous sub
 
 
 def max_subarray(list_):
+    if not list_:
+        return list_, 0
     stop = len(list_)
-    sum_ = 0
-    result = []
+    sum_ = list_[0]
+    result = [sum_]
     start = 1
     for index_start, _ in enumerate(list_):
-        for index_stop in range(start, stop):
+        for index_stop in range(start, stop + 1):
             if sum_ < sum(list_[index_start:index_stop]):
                 sum_ = sum(list_[index_start:index_stop])
                 result = list_[index_start:index_stop]
@@ -21,3 +23,10 @@ def max_subarray(list_):
 
 
 assert max_subarray([-2, 1, -3, 4, -1, 2, 1, -5, 4]) == ([4, -1, 2, 1], 6)
+assert max_subarray([10, -1, -3, -4, -1, 2, 1, -5, 4]) == ([10], 10)
+assert max_subarray([1, -1, -3, -4, -1, 2, 1, -5, 9]) == ([9], 9)
+assert max_subarray([]) == ([], 0)
+assert max_subarray([11]) == ([11], 11)
+assert max_subarray([1, 2, 3, 4]) == ([1, 2, 3, 4], 10)
+assert max_subarray([-1, -2, -3, -4]) == ([-1], -1)
+
